53b107b00e3b4c320ed1df26d01e344c8fa242cf
Quan To <btquanto@gmail.com>
2017-08-11 10:38:11 +0700

    move template files for home blueprint from templates folder to templates/home folder

--- application/blueprints/home/home.py
+++ application/blueprints/home/home.py
@@ -3,8 +3,8 @@ from flask import Blueprint, render_template, jsonify, request
 from jinja2 import TemplateNotFound
 from .. import app
 
-node = Blueprint("home", __name__, template_folder="views")
+node = Blueprint("home", __name__, template_folder="templates")
 
 @node.route("/index")
 def index():
-    return render_template("index.html")
+    return render_template("home/index.html")
