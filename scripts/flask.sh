41d35c53d703e00379fa1c681dcbf1943eea7ec4
Quan To <btquanto@gmail.com>
2017-08-11 10:44:27 +0700

    Rename initialization.py to app.py

--- scripts/flask.sh
+++ scripts/flask.sh
@@ -13,12 +13,12 @@ then
 fi;
 if $DEBUG
 then
-    python3 initialization.py
+    python3 app.py
 else
     if [ "uwsgi_config.ini" ]
     then
         exec uwsgi --ini uwsgi_config.ini --logto ./logs/uwsgi.log
     else
-        exec uwsgi --socket 0.0.0.0:8000 -w initialization:app
+        exec uwsgi --socket 0.0.0.0:8000 -w app:app
     fi;
 fi;
