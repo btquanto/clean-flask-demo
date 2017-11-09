50e1a7bf5c4a7b13bd7c596424f1aaad7f8ec8f4
Quan To <btquanto@gmail.com>
2017-11-09 11:20:07 +0700

    Refactor LoginManager: + Add LoginManager._callback to reduce code duplicates + Allows blueprints to override the callbacks

--- application/core/access/login_manager/login_manager.py
+++ application/core/access/login_manager/login_manager.py
@@ -8,50 +8,77 @@
     Needs more works in order to be more secured
 '''
 from functools import wraps
+from types import SimpleNamespace as Dummy
 from werkzeug.local import LocalProxy
 from flask import (has_request_context, _request_ctx_stack,
-                    current_app, request, session, jsonify)
+                    current_app as app, request, session, jsonify)
 
 from .mixins import AnonymousUserMixin
 
 class LoginManager(object):
 
     def __init__(self, app=None, with_session=True):
+        self.user = LocalProxy(lambda: self._load_current_user())
+        self.blueprints = {}
+        self._with_session = with_session
+        self.anonymous_user = AnonymousUserMixin()
+
         if app is not None:
             self.init_app(app, with_session)
 
     def init_app(self, app, with_session=True):
-        self.user = LocalProxy(lambda: self.load_current_user())
-        self._with_session = with_session
-        self.anonymous_user = AnonymousUserMixin()
+        pass
 
-    def unauthorize_callback(self, func):
-        @wraps
-        def wrapper(*args, **kwargs):
-            return func(*args, **kwargs)
-        self._unauthorize_callback = func
-        return wrapper
+    def _set_callback(self, name, func, blueprint=None):
+        if blueprint:
+            _blueprint = self.blueprints.get(blueprint, Dummy())
+            setattr(_blueprint, name, func)
+            self.blueprints[blueprint] = _blueprint
+        else:
+            setattr(self, name, func)
 
-    def reload_user_callback(self, func):
-        @wraps
-        def wrapper(*args, **kwargs):
-            return func(*args, **kwargs)
-        self._reload_user_callback = func
-        return wrapper
+    def _get_callback(self, name, blueprint=None):
+        if blueprint:
+            _blueprint = self.blueprints.get(blueprint, None)
+            func = getattr(_blueprint, name, None)
+            app.logger.info(func)
+        return func or getattr(self, name, None)
 
-    def login_user_callback(self, func):
-        @wraps
-        def wrapper(*args, **kwargs):
-            return func(*args, **kwargs)
-        self._login_user_callback = func
-        return wrapper
+    def _load_current_user(self):
+        if has_request_context() and not hasattr(_request_ctx_stack.top, 'user'):
+            user = self.reload_user()
+            if user is None:
+                return self.anonymous_user
+            return user
+        return getattr(_request_ctx_stack.top, 'user', self.anonymous_user)
 
-    def user_loader(self, func):
-        @wraps
-        def wrapper(*args, **kwargs):
-            return func(*args, **kwargs)
-        self._load_user = func
-        return wrapper
+    def _callback(self, callback, blueprint):
+
+        def decorator(func):
+            @wraps
+            def wrapper(*args, **kwargs):
+                return func(*args, **kwargs)
+            self._set_callback(callback, func, blueprint)
+            return wrapper
+
+        if callable(blueprint):
+            func = blueprint
+            blueprint = None
+            return decorator(func)
+
+        return decorator
+
+    def unauthorize_callback(self, blueprint=None):
+        return self._callback("_unauthorize_callback", blueprint)
+
+    def reload_user_callback(self, blueprint=None):
+        return self._callback("_reload_user_callback", blueprint)
+
+    def login_user_callback(self, blueprint=None):
+        return self._callback("_login_user_callback", blueprint)
+
+    def user_loader(self, blueprint=None):
+        return self._callback("_user_loader", blueprint)
 
     @property
     def session(self):
@@ -65,23 +92,23 @@ class LoginManager(object):
             return jsonify({
                 "success" : False
             })
-        return getattr(self, '_unauthorize_callback', _unauthorize_callback)
+        return self._get_callback('_unauthorize_callback', request.blueprint) or _unauthorize_callback
 
     @property
     def load_user(self):
-        return getattr(self, '_load_user', lambda : self.anonymous_user)
+        return self._get_callback('_user_loader', request.blueprint) or (lambda : self.anonymous_user)
 
     @property
     def reload_user(self):
         def _reload_user_callback():
-            if self._use_session:
+            if self._with_session:
                 _session = self.session
                 user_id = _session['user_id']
                 user = self.load_user(user_id)
                 if user is not None:
                     return user
             return self.anonymous_user
-        return getattr(self, '_reload_user_callback', _reload_user_callback)
+        return self._get_callback('_reload_user_callback', request.blueprint) or _reload_user_callback
 
     @property
     def login_user(self):
@@ -89,17 +116,7 @@ class LoginManager(object):
             _request_ctx_stack.top.user = user
             self.login_user_callback(user, remember)
             return user
-
-        return getattr(self, '_login_user_callback', _login_user_callback)
-
-    def load_current_user(self):
-        from flask import current_app as app
-        if has_request_context() and not hasattr(_request_ctx_stack.top, 'user'):
-            user = self.reload_user()
-            if user is None:
-                return self.anonymous_user
-            return user
-        return getattr(_request_ctx_stack.top, 'user', self.anonymous_user)
+        return self._get_callback('_login_user_callback', request.blueprint) or _login_user_callback
 
     def login_required(self, func):
         @wraps(func)
