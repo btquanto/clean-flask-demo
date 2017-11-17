cbcf333cff5ead25da7603e5acd4b03caa0b0279
Quan To <btquanto@gmail.com>
2017-11-17 11:51:40 +0700

    Untrack docker-compose.yml and make a template file for it

--- scripts/flask.sh
+++ scripts/flask.sh
@@ -7,6 +7,13 @@ set -e
 
 cd /src
 
+if ! [ -f config/flaskconfig.py ]
+then
+    echo "Configuration file `config/flaskconfig.py` not found"
+    echo "Please create one using `config/flaskconfig.py.template` as the template"
+    exit 0;
+fi;
+
 if [ -f requirements.txt ]
 then
     if ! $SKIP_REQUIREMENTS_CHECK
