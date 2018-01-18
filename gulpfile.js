530d351910089fa1a7e0081bc269e0e8cd60d332
Quan To <btquanto@gmail.com>
2018-01-18 18:04:05 +0700

    Optimize gulp by only compile endpoint js files

--- gulpfile.js
+++ gulpfile.js
@@ -1,38 +1,38 @@
 'use strict';
 
 // dependencies
-var babel = require('gulp-babel');
-var gulp = require('gulp');
-var minifyCSS = require('gulp-clean-css');
-var named = require('vinyl-named');
-var path = require('path');
-var pump = require('pump');
-var rename = require('gulp-rename');
-var sass = require('gulp-sass');
-var uglify = require('gulp-uglify');
-var webpack = require('gulp-webpack');
-var argv = require('yargs').argv;
+const babel = require('gulp-babel');
+const gulp = require('gulp');
+const minifyCSS = require('gulp-clean-css');
+const named = require('vinyl-named');
+const path = require('path');
+const pump = require('pump');
+const rename = require('gulp-rename');
+const sass = require('gulp-sass');
+const uglify = require('gulp-uglify');
+const webpack = require('gulp-webpack');
+const argv = require('yargs').argv;
 
-var SOURCES = [
-    "home",
-    "rtc"
+const SCSS_SOURCES = [
+    'home',
+    'rtc'
 ]
 
-// var JS_SOURCES = {
-//     "home": [
-//         "register",
-//         "reduxrtc",
-//         "webrtc"
-//     ],
-//     "rtc": []
-// }
+const JS_SOURCES = {
+    'home': [
+        'home',
+    ],
+    'rtc': [
+        'rtc'
+    ]
+}
 
-var BUILD_FOLDER = 'static/assets';
+const BUILD_FOLDER = 'static/assets';
 
-var isProduction = (argv.production === undefined) ? false : true;
+const isProduction = (argv.production === undefined) ? false : true;
 
 function compileSCSS() {
-    SOURCES.map(
+    SCSS_SOURCES.map(
         function (blueprint) {
             let source = `./app/blueprints/${blueprint}/static/${blueprint}/scss/*.scss`
             let destination = `${BUILD_FOLDER}/${blueprint}/css/`;
@@ -61,92 +61,100 @@ function compileSCSS() {
 }
 
 function compileJS() {
-    SOURCES.map(function (blueprint) {
 
-        let sources = [
-            `./app/blueprints/${blueprint}/static/${blueprint}/js/*.js`,
-            `./app/blueprints/${blueprint}/static/${blueprint}/js/*.jsx`
-        ]
-        let destination = `${BUILD_FOLDER}/${blueprint}/js/`;
+    Object.keys(JS_SOURCES).map(blueprint => {
+        JS_SOURCES[blueprint].map(endpoint => {
+            let sources = [
+                `./app/blueprints/${blueprint}/static/${blueprint}/js/${endpoint}.js`,
+                `./app/blueprints/${blueprint}/static/${blueprint}/js/${endpoint}.jsx`
+            ]
 
-        let pipes = [
-            gulp.src(sources),
-            named(),
-            webpack({
-                module: {
-                    loaders: [{
-                        test: /\.(js|jsx)$/,
-                        loader: 'babel-loader',
-                        query: {
-                            presets: ['es2015', 'react', 'stage-3'],
-                            compact: false,
-                            plugins: ["transform-object-rest-spread"],
-                            cacheDirectory: true
+            let destination = `${BUILD_FOLDER}/${blueprint}/js/`;
+
+            let pipes = [
+                gulp.src(sources),
+                named(),
+                webpack({
+                    module: {
+                        loaders: [{
+                            test: /\.(js|jsx)$/,
+                            loader: 'babel-loader',
+                            query: {
+                                presets: ['es2015', 'react', 'stage-3'],
+                                compact: false,
+                                plugins: ["transform-object-rest-spread"],
+                                cacheDirectory: true
+                            }
+                        },
+                        {
+                            test: /\.css$/,
+                            loader: "style-loader!css-loader"
+                        },
+                        {
+                            test: /\.(woff|woff2)(\?v=\d+\.\d+\.\d+)?$/,
+                            loader: 'url-loader?limit=10000&mimetype=application/font-woff'
+                        },
+                        {
+                            test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
+                            loader: 'url-loader?limit=10000&mimetype=application/octet-stream'
+                        },
+                        {
+                            test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
+                            loader: 'file-loader'
+                        },
+                        {
+                            test: /\.svg$/,
+                            loader: 'svg-loader'
+                        }
+                        ]
+                    },
+                    resolve: {
+                        modules: ['./node_modules'],
+                        alias: {
+                            Components: path.resolve(__dirname, "components/js")
                         }
                     },
-                    {
-                        test: /\.css$/,
-                        loader: "style-loader!css-loader"
-                    },
-                    {
-                        test: /\.(woff|woff2)(\?v=\d+\.\d+\.\d+)?$/,
-                        loader: 'url-loader?limit=10000&mimetype=application/font-woff'
-                    },
-                    {
-                        test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
-                        loader: 'url-loader?limit=10000&mimetype=application/octet-stream'
-                    },
-                    {
-                        test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
-                        loader: 'file-loader'
-                    },
-                    {
-                        test: /\.svg$/,
-                        loader: 'svg-loader'
+                    stats: {
+                        colors: true
                     }
-                    ]
-                },
-                resolve: {
-                    modules: ['./node_modules'],
-                    alias: {
-                        Components: path.resolve(__dirname, "components/js")
-                    }
-                },
-                stats: {
-                    colors: true
-                }
-            }),
-            rename({
-                suffix: '.min'
-            }),
-            gulp.dest(destination)
-        ];
+                }),
+                rename({
+                    suffix: '.min'
+                }),
+                gulp.dest(destination)
+            ];
 
-        if (isProduction) {
-            pipes.splice(pipes.length - 2, 0, uglify());
-        }
+            if (isProduction) {
+                pipes.splice(pipes.length - 2, 0, uglify());
+            }
 
-        return pump(pipes);
+            pump(pipes);
+        })
     });
 }
 
 function watch() {
-    SOURCES.map(
+    SCSS_SOURCES.map(
         function (blueprint) {
             let source = `./app/blueprints/${blueprint}/static/${blueprint}/scss/*.scss`
             let destination = `${BUILD_FOLDER}/${blueprint}/css/`;
-            return gulp.watch(source, ['compile_scss'])
+            gulp.watch(source, ['compile_scss'])
         }
     );
-    SOURCES.map(function (blueprint) {
+
+    Object.keys(JS_SOURCES).map(function (blueprint) {
         let sources = [
-            './components/js/*.js',
-            './components/js/*.jsx',
             `./app/blueprints/${blueprint}/static/${blueprint}/js/*.js`,
-            `./app/blueprints/${blueprint}/static/${blueprint}/js/*.jsx`
         ]
-        return gulp.watch(sources, ['compile_js'])
+        gulp.watch(sources, ['compile_js'])
     });
+
+    let components_sources = [
+        './components/js/*.js',
+        './components/js/*.jsx',
+    ];
+
+    gulp.watch(components_sources, ['compile_js'])
 }
 
 gulp.task('compile_scss', compileSCSS);
