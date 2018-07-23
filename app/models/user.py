ba8a5559c7c69df55f44d27ae5125ca2791ca8b4
Quan To <btquanto@gmail.com>
2018-07-23 17:39:52 +0700

    XXX

--- app/models/user.py
+++ app/models/user.py
@@ -2,10 +2,17 @@
 import secrets
 from datetime import datetime, timedelta
 from flask import current_app as app
-from flask_login import UserMixin as LoginUserMixin
+from core.access.login_manager.mixins import UserMixin as LoginUserMixin
+from core.access.rnac.models import UserMixin as RbacUserMixin
 from app import db
 
-class User(db.Model, LoginUserMixin):
+users_roles = db.Table(
+    'users_roles',
+    db.Column('user_id', db.Integer, db.ForeignKey('users.id')),
+    db.Column('role_id', db.Integer, db.ForeignKey('roles.id'))
+)
+
+class User(db.Model, LoginUserMixin, RbacUserMixin):
     __tablename__ = "users"
 
     id = db.Column(db.Integer, primary_key=True)
@@ -13,6 +20,23 @@ class User(db.Model, LoginUserMixin):
     username = db.Column(db.String(255))
     email = db.Column(db.String(255))
     password = db.Column(db.String(255))
+
+    roles = db.relationship(
+        'Role',
+        secondary=users_roles,
+        backref=db.backref('roles', lazy='dynamic')
+    )
+
+    def add_role(self, role):
+        self.roles.append(role)
+
+    def add_roles(self, roles):
+        for role in roles:
+            self.add_role(role)
+
+    def get_roles(self):
+        for role in self.roles:
+            yield role
 
     def as_dict(self):
         return {
---
+++ application.py
@@ -0,0 +1,54 @@
+# -*- coding: utf-8 -*-
+import eventlet, logging
+from flask import Flask, redirect, url_for, send_from_directory
+from app import db, lm, session
+from app import blueprints
+from app.models import User
+from core.reverse_proxied import ReverseProxied
+
+# Configuration
+eventlet.monkey_patch()
+
+app = Flask(__name__)
+app.config.from_object("config.default")
+try:
+    app.config.from_object("config.local")
+except Exception as ignored:
+    pass
+
+app.wsgi_app = ReverseProxied(app.wsgi_app)
+
+# SQLAlchemy
+db.init_app(app)
+
+# Flask-Session
+session.init_app(app)
+
+# Login Managers
+def load_user_by_id(user_id):
+    from flask import current_app as app
+    app.logger.debug("Loading user")
+    user = User.query.get(user_id)
+    app.logger.debug(user)
+    return user
+
+lm.init_app(app, with_session=True)
+lm.user_loader(load_user_by_id)
+
+# Register blueprints
+app.register_blueprint(blueprints.api.node, url_prefix="/api")
+
+# Logger
+gunicornLogging = logging.getLogger('gunicorn.error')
+fileLogging = logging.FileHandler('logs/flask.log')
+
+app.logger.handlers.extend(gunicornLogging.handlers)
+app.logger.addHandler(fileLogging)
+app.logger.addHandler(logging.StreamHandler())
+
+app.logger.setLevel(logging.DEBUG)
+
+# Default route
+@app.route('/')
+def bootstrap():
+    return redirect('/app/')
